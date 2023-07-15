c97ebab61548c19529451c39613424cfd50c6ed2
Penn Wilbert <pwilbert@gmail.com>
2023-07-15 06:59:44 -0400

    added converter

--- src/main.py
+++ src/main.py
@@ -2,12 +2,14 @@ from tkinter import *
 from tkinter import ttk, filedialog
 import splitter
 import silencer
+import converter
 
 def run_silencer():
     try:
         src_dir = filedialog.askdirectory(title="Source Directory")
         dst_dir = filedialog.askdirectory(title="Destination Directory")
-        silencer.run(src_dir, dst_dir)
+        if src_dir != None and dst_dir != None:
+            silencer.run(src_dir, dst_dir)
     except Exception as err:
         print(err)
     
@@ -18,6 +20,18 @@ def run_splitter():
     except Exception as err:
         print(err)
 
+
+def run_converter():
+    try:
+        src_dir = filedialog.askdirectory(title="Source Directory")
+        dst_dir = filedialog.askdirectory(title="Destination Directory")
+        if src_dir != None and dst_dir != None:
+            converter.run(src_dir, dst_dir)
+    except Exception as err:
+        print(err)
+
+
+
 def main():
     root = Tk()
     root.maxsize(200, 200)
@@ -27,6 +41,8 @@ def main():
     splitter_button.pack(padx=10, pady=10)
     silencer_button = ttk.Button(frm, text="Silencer", command=run_silencer)
     silencer_button.pack(padx=10, pady=10)
+    converter_button = ttk.Button(frm, text="Converter", command=run_silencer)
+    converter_button.pack(padx=10, pady=10)
     frm.pack(padx=10, pady=10)
     root.mainloop()
 
